b120b8a4be4c86e34a04b7d48ab798a8dc51b933
kuniczuk <mateusz.kuniczuk@nokia.com>
2026-02-24 14:14:03 +0100

    stub for ThreadPool

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use std::{fs, io::{BufReader, prelude::*}, net::{TcpListener, TcpStream}};
+use std::{fs, io::{BufReader, prelude::*}, net::{TcpListener, TcpStream}, thread};
+use std::time::Duration;
+use web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -14,10 +19,14 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "static/hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "static/404.html")
+    // match doesn’t do automatic referencing and dereferencing, like the equality method does
+    let (status_line, filename) = match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "static/hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "static/hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "static/404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
